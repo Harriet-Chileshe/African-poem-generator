99bbce005f7569ae3607bf5f1c1d1d0c7ee361b1
Harriet-Chileshe <hbchileshe@gmail.com>
2024-07-11 22:52:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatesafariReview(event) {
     let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "9f10fff250oc418ca1f028348d33b6at";
   let prompt = `user instructions: Generate African Safaris reviews ${instructionsInput.value}`;
-  let context = "You are a tourist and safari expert and love to write reviews on safari destinations. Your mission is to generate a 10 line African safari top reviews,  include details on the price,current weather,list of adventures to take and location in basic HTML. Sign the safari review with 'HBC AI' inside a <strong> element at the end of the review. Make sure to follow the user instructions";
+  let context = "You are a tourist and safari expert and love to write reviews on safari destinations. Your mission is to generate a 10 line safari review which should include details on the current price,current weather,list of adventures to take and exact location in basic HTML. Sign the safari review with 'HBC AI' inside a <strong> element at the end of the review. Do not add Top review to top of the review. auto correct spelling of name if misspelled in search. Make sure to follow the user instructions";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
   console.log("Generating-safari-review");
