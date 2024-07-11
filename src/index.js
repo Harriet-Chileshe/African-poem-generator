b384755b2f1eb9d35fa80897532c7dfbfccd17dc
Harriet-Chileshe <hbchileshe@gmail.com>
2024-07-11 22:28:14 +0200

    Renamed repository to African safaris and updated all files with new name details

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-function displayPoem(response) {
-    console.log("poem generated");
+function displayReview(response) {
+    console.log("safari-review generated");
 
-   new Typewriter("#poem", {
+   new Typewriter("#safari-review", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 10,
+    delay: 5,
     cursor: "",
   });   
 }
 
-function generatePoem(event) {
+function generatesafariReview(event) {
   event.preventDefault();
 
     let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "9f10fff250oc418ca1f028348d33b6at";
-  let prompt = `user instructions: Generate African poem about ${instructionsInput.value}`;
-  let context = "You are a culture poem expert and love to write short poems. Your mission is to generate a 5 line poem in basic HTML. Sign the poem with 'HBC AI' inside a <strong> element at the end of the poem. Make sure to follow the user instructions";
+  let prompt = `user instructions: Generate African Safaris reviews ${instructionsInput.value}`;
+  let context = "You are a tourist and safari expert and love to write reviews on safari destinations. Your mission is to generate a 10 line African safari top reviews,  include details on the price,current weather,list of adventures to take and location in basic HTML. Sign the safari review with 'HBC AI' inside a <strong> element at the end of the review. Make sure to follow the user instructions";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
 
-  console.log("Generating-poem");
+  console.log("Generating-safari-review");
   console.log(`prompt: ${prompt}`);
   console.log(`context: ${context}`);
 
-  axios.get(apiURL).then(displayPoem);
+  axios.get(apiURL).then(displayReview);
 
 
 }
 
-let poemFormElement = document.querySelector("#poem-generator");
-poemFormElement.addEventListener("submit", generatePoem);
+let safariFormElement = document.querySelector("#safari-generator");
+safariFormElement.addEventListener("submit", generatesafariReview);
