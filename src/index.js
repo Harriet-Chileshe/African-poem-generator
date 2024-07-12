6b6bc92969617a21e674378fc37608146769c1a2
Harriet-Chileshe <hbchileshe@gmail.com>
2024-07-12 17:25:33 +0200

    removed  console.log

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
 function displayListing(response) {
-    console.log("safari-listing generated");
-
-   new Typewriter("#safari-listing", {
+  new Typewriter("#safari-listing", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
-  });   
+  });
 }
 
 function generatesafariListing(event) {
   event.preventDefault();
 
-    let instructionsInput = document.querySelector("#user-instructions");
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "9f10fff250oc418ca1f028348d33b6at";
   let prompt = `user instructions: Generate African Safari listing ${instructionsInput.value}`;
-  let context = "You are a tourist and safari expert and love to write on safari destinations. Your mission is to generate a list 2 top safaris in any African country,please include details on the safari's current price,location in that country(town),current weather and the list of adventures to take in basic HTML. If a country been searched has no safaris kindly indicate no safaris in this country.Sign the safari listing at the end with 'HBC AI' inside a <strong> element at the end of the review. Auto-correct names of countries that may be misspelled in search. Make sure to follow the user instructions";
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+  let context =
+    "You are a tourist and safari expert and love to write on safari destinations. Your mission is to generate a list 2 top safaris in any African country,please include details on the safari's current price,location in that country(town),current weather and the list of adventures to take in basic HTML. If a country been searched has no safaris kindly indicate no safaris in this country.Sign the safari listing at the end with 'HBC AI' inside a <strong> element at the end of the review. Auto-correct names of countries that may be misspelled in search. Make sure to follow the user instructions";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let safariListingElement = document.querySelector("#safari-listing");
   safariListingElement.classList.remove("hidden");
   safariListingElement.innerHTML = `<div class="blink">⏳ Generating Safaris in ${instructionsInput.value}.....</div>`;
 
-  console.log("Generating-safari-listing");
-  console.log(`prompt: ${prompt}`);
-  console.log(`context: ${context}`);
-
   axios.get(apiURL).then(displayListing);
-
-
 }
 
 let safariFormElement = document.querySelector("#safari-generator");
